ec37fb7800801afb7d583f2e67f427b1aafba1f7
Lixeer <1612655510@qq.com>
2026-02-13 16:38:45 +0800

    test:测试GitHub build

--- Mods/CommandWebUI/ModConfig.cs
+++ Mods/CommandWebUI/ModConfig.cs
@@ -3,7 +3,7 @@ namespace CommandWebUI
     public class ModConfig
     {
         public int Port = 29103;
-        public string AccessToken = "24641aabcd";
+        public string AccessToken = "set_your_own_token_please";
         public string IndexPage = "index-sv2.html";
         public string LoginPage = "Login.html";
     }
